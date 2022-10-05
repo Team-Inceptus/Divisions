c21f4f213659a1ae8d0d04a8b72818981efff265
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-05 00:35:48 -0500

    Update Divisions.java

--- plugin/src/main/java/me/gamercoder215/divisions/Divisions.java
+++ plugin/src/main/java/me/gamercoder215/divisions/Divisions.java
@@ -1,16 +1,21 @@
 package me.gamercoder215.divisions;
 
-import me.gamercoder215.divisions.api.DivisionsConfig;
+import me.gamercoder215.divisions.api.DivConfig;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Constructor;
+import java.util.Properties;
 import java.util.logging.Logger;
 
-public final class Divisions extends JavaPlugin implements DivisionsConfig {
+public final class Divisions extends JavaPlugin implements DivConfig {
 
     private static Logger LOGGER;
+    private static FileConfiguration config;
 
     private static boolean adventureEnabled = false;
 
@@ -25,15 +30,49 @@ public final class Divisions extends JavaPlugin implements DivisionsConfig {
             adventureEnabled = true;
         } catch (ClassNotFoundException ignored) {
         } catch (ReflectiveOperationException e) {
-            DivisionsConfig.print(e);
+            DivConfig.print(e);
         }
     }
 
     @Override
     public void onEnable() {
         LOGGER = getLogger();
+        saveDefaultConfig();
+
+        getLogger().info("Loading Files...");
+        config = getConfig();
 
         getLogger().info("Done!");
+    }
+
+    // Configuration Implementation
+
+    @Override
+    public String get(String key) {
+        if (key == null) return null;
+        String id = getLanguage().equalsIgnoreCase("en") ? "" : "_" + getLanguage() ;
+
+        Properties p = new Properties();
+        try (InputStream str = DivConfig.getPlugin().getClass().getResourceAsStream("/lang/divisions" + id + ".properties")) {
+            if (str == null) throw new IOException("Unknown Language: " + id);
+
+            p.load(str);
+            str.close();
+            return ChatColor.translateAlternateColorCodes('&', p.getProperty(key, "Unknown Value"));
+        } catch (IOException e) {
+            DivConfig.print(e);
+            return "Unknown Value";
+        }
+    }
+
+    @Override
+    public String getMessage(String key) {
+        return get("plugin.prefix") + " " + get(key);
+    }
+
+    @Override
+    public String getLanguage() {
+        return null;
     }
 
     @Override
