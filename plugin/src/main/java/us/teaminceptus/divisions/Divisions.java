a2a94e94a9dae7c5bd1720b241244f74fa350517
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-12-27 22:15:38 -0600

    Create Division#checkCompatible

--- plugin/src/main/java/us/teaminceptus/divisions/Divisions.java
+++ plugin/src/main/java/us/teaminceptus/divisions/Divisions.java
@@ -1,5 +1,6 @@
 package us.teaminceptus.divisions;
 
+import org.bukkit.Bukkit;
 import us.teaminceptus.divisions.api.DivConfig;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,15 +11,42 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Logger;
 
+import static us.teaminceptus.divisions.wrapper.Wrapper.isCompatible;
+import static us.teaminceptus.divisions.wrapper.Wrapper.isOutdatedSubversion;
+
 public final class Divisions extends JavaPlugin implements DivConfig {
 
     private static Logger LOGGER;
     private static FileConfiguration config;
 
+    private boolean checkCompatible() {
+        if (isOutdatedSubversion()) {
+            LOGGER.severe("** This subversion of Minecraft is not compatible with Divisions! **");
+            LOGGER.severe("** Please update your MC to the latest version. **");
+
+            Bukkit.getPluginManager().disablePlugin(this);
+            return false;
+        }
+
+        if (!isCompatible()) {
+            LOGGER.severe("** This version of Minecraft is not compatible with Divisions! **");
+            LOGGER.severe("** Please update the Divisions plugin or Minecraft to the latest version. **");
+
+            Bukkit.getPluginManager().disablePlugin(this);
+            return false;
+        }
+
+        return true;
+    }
+
     @Override
     public void onEnable() {
+        if (!checkCompatible()) return;
+
         LOGGER = getLogger();
         saveDefaultConfig();
+
+
 
         getLogger().info("Loading Files...");
         config = getConfig();
