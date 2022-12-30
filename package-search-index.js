60cf33ff0407e60cff45ebe85c7f72dc30d2f2b1
GitHub Action <action@github.com>
2022-12-30 06:20:43 +0000

    Update JavaDocs (04913f4)

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"us.teaminceptus.divisions"},{"l":"us.teaminceptus.divisions.api"},{"l":"us.teaminceptus.divisions.api.division"},{"l":"us.teaminceptus.divisions.api.player"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"us.teaminceptus.divisions"},{"l":"us.teaminceptus.divisions.api"},{"l":"us.teaminceptus.divisions.api.division"},{"l":"us.teaminceptus.divisions.api.division.logs"},{"l":"us.teaminceptus.divisions.api.player"}];updateSearchResults();
