677931d6f5f112957afc80a7aaa804e7598ba6bd
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-12-31 01:23:00 -0600

    Draft DivPlayer Division API

--- api/src/main/java/us/teaminceptus/divisions/api/player/DivPlayer.java
+++ api/src/main/java/us/teaminceptus/divisions/api/player/DivPlayer.java
@@ -1,10 +1,16 @@
 package us.teaminceptus.divisions.api.player;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 import us.teaminceptus.divisions.api.DivConfig;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
+import us.teaminceptus.divisions.api.division.Division;
+import us.teaminceptus.divisions.api.events.player.PlayerMessageEvent;
 
 import java.io.File;
 
@@ -61,4 +67,46 @@ public final class DivPlayer {
     public FileConfiguration getConfig() {
         return config;
     }
+
+    /**
+     * Fetches the Division this DivPlayer is in.
+     * @return Division
+     * @since 1.0.0
+     */
+    @Nullable
+    public Division getDivision() {
+        return Division.byOwner(p) == null ? Division.byMember(p) : null;
+    }
+
+    /**
+     * Determines whether this DivPlayer is the owner of the Division they are in.
+     * @return true if owner, false otherwise
+     * @since 1.0.0
+     */
+    public boolean isDivisionOwner() {
+        return getDivision() != null && getDivision().getOwner().getUniqueId().equals(p.getUniqueId());
+    }
+
+    /**
+     * <p>Sends a message in the Divisions Chat Format.</p>
+     * <p>This method will fail (do nothing) if the player is not online.</p>
+     * @param message Message to send
+     */
+    public void sendDivisionMessage(@NotNull String message) {
+        if (!p.isOnline()) return;
+        Division d = getDivision();
+        if (d == null) return;
+
+        Player pl = p.getPlayer();
+        PlayerMessageEvent event = new PlayerMessageEvent(d, pl, message);
+        Bukkit.getPluginManager().callEvent(event);
+
+        if (event.isCancelled()) return;
+
+        String plPrefix = pl.getDisplayName() == null ? pl.getName() : pl.getDisplayName() + ChatColor.RESET;
+        String prefix = plPrefix + ChatColor.GOLD + "[" + (isDivisionOwner() ? "O" : "M") + "]" + ChatColor.RESET + ": ";
+
+        d.broadcastMessage(prefix + event.getMessage());
+    }
+
 }
